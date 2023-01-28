68c15efff356fd24ce98a9deab17f28a37a915bf
Cem İntepe <109594332+cemintepe@users.noreply.github.com>
2023-01-28 11:26:27 +0000

    Update artikyil.java

--- artikyil.java
+++ artikyil.java
@@ -8,7 +8,7 @@ public class test3 {
         System.out.print("Yil Giriniz: ");
         yil = input.nextInt();
 
-        if (yil % 400 == 0){
+        if ((yil % 400 == 0) && (yil % 100 == 0) && (yil % 4 == 0)){
             System.out.print(yil + " bir Artık Yıldır.");
         }else {
             System.out.print(yil + "  bir Artik Yıl Değildir");
