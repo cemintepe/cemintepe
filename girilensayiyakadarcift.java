4021e3405f7cea0baccbb4049257160a18617a05
Cem İntepe <109594332+cemintepe@users.noreply.github.com>
2023-01-28 18:48:50 +0000

    Create girilensayiyakadarcift.java

---
+++ girilensayiyakadarcift.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class test3 {
+    public static void main(String[] args) {
+        int k;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz: ");
+        k = input.nextInt();
+
+        for(int i = 1 ; i <= k ; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+        }
+    }
